1f545501c499ef99496e60787b889dbe2924e3f6
Nodragem <geoffrey.megardon@gmail.com>
2018-09-29 21:52:51 +0100

    first commit

---
+++ src/MainMenu.ts
@@ -0,0 +1,189 @@
+import * as BABYLON from 'babylonjs';
+import * as BGUI from 'babylonjs-gui';
+import {Tween} from "./Tween";
+
+export class MainMenu {
+    private _scene:BABYLON.Scene;
+    private _panel:BGUI.Rectangle;
+    private _tweenManager:BABYLON.Animatable;
+
+    constructor(scene : BABYLON.Scene) {
+        this._scene = scene;
+
+        let advancedTexture = BGUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        
+        this._panel = new BGUI.Rectangle();
+        this._panel.width = 0.8;
+        this._panel.height = 0.6;
+        this._panel.cornerRadius = 20;
+        this._panel.color = "#73938D";
+        this._panel.thickness = 4;
+        this._panel.background = "#EEECE7";
+        advancedTexture.addControl(this._panel);  
+        
+        let buttonSize = "100px";
+        let button = BGUI.Button.CreateImageWithCenterTextButton("but", "RT", "assets/2D/big_arrow_right@2x.png");
+        button.verticalAlignment = BGUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button.width = buttonSize;
+        button.height = buttonSize;
+        button.left = 200;
+        button.top = 0;
+        button.fontSize = "36px";
+        button.color = "#ffffff";
+        button.thickness = 0;
+        
+        this._panel.addControl(button);
+        this._tweenManager = Tween.createTween1D(this._scene, button, "left", 1000, 200, 
+                            .5, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE,
+                            new BABYLON.BounceEase(1, 10),
+                            BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+        
+        button = BGUI.Button.CreateImageWithCenterTextButton("but", "LT", "assets/2D/big_arrow_left@2x.png");
+        button.verticalAlignment = BGUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button.width = buttonSize;
+        button.height = buttonSize;
+        button.left = -200;
+        button.top = 0;
+        button.fontSize = "36px";
+        button.color = "#ffffff";
+        //button.fontFamily = "";
+        button.thickness = 0;
+        
+        this._panel.addControl(button);
+        Tween.addTween1D(this._tweenManager, button, "left", -1000, -200, 
+        .5, BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE,
+        new BABYLON.BounceEase(1, 10),
+        BABYLON.EasingFunction.EASINGMODE_EASEOUT);
+        
+        button = BGUI.Button.CreateSimpleButton("button1", "LEVEL SELECTION");
+        button.verticalAlignment = BGUI.Control.VERTICAL_ALIGNMENT_TOP;
+        button.width = 0.5;
+        button.top = 10;
+        button.cornerRadius = 10;
+        button.thickness = 4;
+        button.height = "40px";
+        button.color = "#00738E";
+        button.background = "#2C89A0";
+        
+        this._panel.addControl(button);
+        
+        
+        let horStack = new BGUI.StackPanel();
+        horStack.verticalAlignment = BGUI.Control.VERTICAL_ALIGNMENT_TOP;
+        horStack.top = "70px";
+        horStack.width = 1;
+        horStack.height = 0.7;
+        horStack.isVertical = false;
+        
+        this._panel.addControl(horStack); 
+
+        let grid1 = this.createGrid(2, 1);
+        grid1.paddingLeft = grid1.paddingRight = 0;
+        grid1.width = 0.5;
+        grid1.height = 1;
+        horStack.addControl(grid1);
+        
+        let rect = new BGUI.Rectangle();
+        rect.background = "#504738";
+        rect.color = "#2E0D00";
+        rect.cornerRadius = 10;
+        rect.thickness = 1;
+        let grid2 = this.createGrid(10, 2, rect, [0.5], [0.6, 0.4]);
+        grid2.paddingLeft = grid2.paddingRight = 0;
+        grid2.width = 0.5;
+        grid2.height = 1;
+        horStack.addControl(grid2);
+
+        let row;
+        for(row = 0; row < 10; row++){
+            grid2.addControl(new Selector("Var"+row), row, 1);
+        }   
+        this.show(false);
+    }
+
+    private createGrid(nrow:number, ncol:number, cellObject?:BGUI.Control, 
+        rowsizes?:number[], colsizes?:number[]) : BGUI.Grid {
+    
+        if(cellObject === undefined){
+            let rect = new BGUI.Rectangle();
+            rect.background = "white";
+            rect.color = "black";
+            rect.cornerRadius = 20;
+            rect.thickness = 1;
+            cellObject = rect;
+        }
+
+        let grid = new BGUI.Grid();   
+        grid.background = "#00000000"; 
+        
+        let col;
+        for (col = 0; col < ncol; col++){
+            if(colsizes)
+                grid.addColumnDefinition(colsizes[col % colsizes.length]);
+            else
+                grid.addColumnDefinition(0.5);
+        }
+        let row;
+        for (row = 0; row < nrow; row++){
+            if(rowsizes)
+                grid.addRowDefinition(rowsizes[row % rowsizes.length]);
+            else
+                grid.addRowDefinition(0.5);
+            }
+        
+        for (row = 0; row < nrow; row++){
+            for (col = 0; col < nrow; col++){
+                grid.addControl(cellObject, row, col);     
+            }
+        }
+        return grid
+    }
+
+    public show(b:boolean){
+        this._panel.isVisible = b;
+        if(b){
+            this._tweenManager.reset();
+            this._tweenManager.restart();
+        }
+        else{
+            this._tweenManager.reset();
+            this._tweenManager.pause();
+        }
+
+    }
+
+    public toggle(){
+        this.show(!this._panel.isVisible);
+    }
+}
+
+class Selector extends BGUI.Container {
+    buttonUp:BGUI.Button;
+    buttonDown:BGUI.Button;
+    varText:BGUI.TextBlock;
+    buttonSize:string = "32px";
+    constructor(initText:string){
+        super();
+        this.buttonUp = BGUI.Button.CreateImageOnlyButton("but", "assets/2D/small_arrow_right@2x.png");
+        this.buttonUp.horizontalAlignment = BGUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        this.buttonUp.width = this.buttonSize;
+        this.buttonUp.height = this.buttonSize;
+        this.buttonUp.thickness = 0;
+    
+        this.buttonDown = BGUI.Button.CreateImageOnlyButton("but", "assets/2D/small_arrow_left@2x.png");
+        this.buttonDown.horizontalAlignment = BGUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        this.buttonDown.width = this.buttonSize;
+        this.buttonDown.height = this.buttonSize;
+        this.buttonDown.thickness = 0;
+        
+        this.varText = new BGUI.TextBlock();
+        this.varText.horizontalAlignment = BGUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+        this.varText.text = initText;
+        this.varText.color = "white";
+
+        this.addControl(this.buttonUp);
+        this.addControl(this.buttonDown);
+        this.addControl(this.varText);
+
+    }
+}
---
+++ src/game.ts
@@ -0,0 +1,144 @@
+import * as BABYLON from 'babylonjs';
+import * as BGUI from 'babylonjs-gui';
+import {Tween} from './Tween';
+import {MainMenu} from './MainMenu';
+
+class Game {
+    private _canvas: HTMLCanvasElement;
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
+    private _camera: BABYLON.FreeCamera;
+    private _light: BABYLON.Light;
+    private _ground: BABYLON.Mesh;
+    private _mainMenu: MainMenu;
+
+    constructor(canvasElement : string) {
+        // Create canvas and engine.
+        this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+        this._engine = new BABYLON.Engine(this._canvas, true);    
+        // Create the scene.
+        this.createScene();
+            
+        // Create a Menu
+        this.createMenu();
+        
+        // attach Keyboard.
+        this.attachKeyboard();
+        
+        // Start render loop.
+        this.doRender();        
+    }
+
+    createMenu() : void {
+        this._mainMenu = new MainMenu(this._scene);
+        this._mainMenu.show(true);
+
+    }
+
+    attachKeyboard(){
+        this._scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.type) {
+                case BABYLON.KeyboardEventTypes.KEYDOWN:
+                    switch (kbInfo.event.key) {
+                        case "a":
+                        case "A":
+                        case " ":
+                            this._mainMenu.toggle();
+                        break
+                    }
+                break;
+            }
+        });
+    
+    }
+    
+    createScene() : void {
+        // Create a basic BJS Scene object.
+        this._scene = new BABYLON.Scene(this._engine);
+    
+        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
+        this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), this._scene);
+        
+        // Target the camera to scene origin.
+        this._camera.setTarget(BABYLON.Vector3.Zero());
+        
+        // Attach the camera to the canvas.
+        this._camera.attachControl(this._canvas, false);
+        
+        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
+        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
+        
+        // Create a built-in "sphere" shape; with 16 segments and diameter of 2.
+        let sphere = BABYLON.MeshBuilder.CreateSphere('sphere1',
+                                {segments: 16, diameter: 2}, this._scene);
+
+        let material = new BABYLON.StandardMaterial("sphere_material", this._scene);
+        material.diffuseColor = new BABYLON.Color3(1, 0.5, 0.5);
+        sphere.material = material;
+
+        // Move the sphere upward 1/2 of its height.
+        sphere.position.y = 1;
+
+        // Create a built-in "ground" shape.
+        this._ground = BABYLON.MeshBuilder.CreateGround('ground1',
+                                {width: 6, height: 6, subdivisions: 2}, this._scene);
+        
+        // the path to the texture corresponds to the path after you build your project (npm run build)
+        material = new BABYLON.StandardMaterial("ground1_material", this._scene);
+        material.diffuseTexture = new BABYLON.Texture("assets/2D/dungeons_and_flagons3.jpg", this._scene);
+        this._ground.material = material;
+
+        let frameRate = 6;
+        let duration = 6;
+
+        let xSlide = new BABYLON.Animation("xSlide", "position", frameRate, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+        let keyFrames = []; 
+
+        keyFrames.push({
+            frame: 0,
+            value: BABYLON.Vector3.Zero(),
+            outTangent: new BABYLON.Vector3(1, 0, 0)
+            //interpolation: BABYLON.AnimationKeyInterpolation.STEP
+        });
+
+        keyFrames.push({
+            frame: duration/2,
+            inTangent: new BABYLON.Vector3(-1, 0, 0),
+            value: new BABYLON.Vector3(0, 0, 1),
+            outTangent: new BABYLON.Vector3(-1, 0, 0)
+            //interpolation: BABYLON.AnimationKeyInterpolation.STEP
+        });
+
+        keyFrames.push({
+            frame: duration,
+            value: BABYLON.Vector3.Zero(),
+            inTangent: new BABYLON.Vector3(1, 0, 0)
+            //interpolation: BABYLON.AnimationKeyInterpolation.STEP
+        });
+
+        xSlide.setKeys(keyFrames);
+
+        let anim = this._scene.beginDirectAnimation(sphere, [xSlide], 0, duration, true);
+        anim.speedRatio = .5;
+
+    }
+
+    doRender() : void {
+        // Run the render loop.
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+
+        // The canvas/window resize event handler.
+        window.addEventListener('resize', () => {
+            this._engine.resize();
+        });
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Create the game using the 'renderCanvas'.
+    let game = new Game('renderCanvas');
+
+});
